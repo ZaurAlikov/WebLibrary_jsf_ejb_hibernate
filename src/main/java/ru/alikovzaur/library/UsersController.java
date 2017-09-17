571e3a3b205577f103eb17c6c8c27d1e784d11b7
Zaur Alikov <ochenmnogopisem@gmail.com>
2017-09-17 17:31:09 +0300

    new UsersController

--- src/main/java/ru/alikovzaur/library/UsersController.java
+++ src/main/java/ru/alikovzaur/library/UsersController.java
@@ -1,59 +1,69 @@
 package ru.alikovzaur.library;
 
-import org.apache.commons.lang3.StringUtils;
-
-import javax.enterprise.context.RequestScoped;
+import javax.annotation.Resource;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
+import javax.transaction.UserTransaction;
 import java.io.Serializable;
-import java.util.List;
 
-//@Named
-//@RequestScoped
+@Named
+@SessionScoped
 public class UsersController implements Serializable {
+    private String login;
+    private String password;
 
-        @PersistenceContext(unitName = "libraryPU")
-        private EntityManager entityManager;
+    @PersistenceContext(unitName = "libraryPU")
+    private EntityManager entityManager;
+    @Resource
+    private UserTransaction userTransaction;
 
-        public boolean checkPassword(String login, String password){
-//            if(StringUtils.isEmpty(login) || StringUtils.isEmpty(password)){
-//                return false;
-//            }
-
-            UsersEntity usersEntity = entityManager.find(UsersEntity.class, login);
-            if(usersEntity == null){
-                return false;
-            }
-
-            if(password.equals(usersEntity.getPassword())){
-                return true;
-            }
-
-            return false;
-        }
-
-        public boolean createUser(String login, String password){
-//            if(StringUtils.isEmpty(login) || StringUtils.isEmpty(password)){
-//                return false;
-//            }
-
-            UsersEntity usersEntity = entityManager.find(UsersEntity.class, login);
-            if(usersEntity != null){
-                return false;
-            }
-
-            usersEntity = new UsersEntity();
-            usersEntity.setLogin(login);
-            usersEntity.setPassword(password);
-            entityManager.persist(usersEntity);
-
-            return true;
-        }
-
-        public List<UsersEntity> getAllUsers(){
-            Query query = entityManager.createQuery("select entity from UsersEntity entity");
-            return query.getResultList();
-        }
+    public String getLogin() {
+        return login;
     }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String checkPassword(){
+        UsersEntity usersEntity = entityManager.find(UsersEntity.class, login);
+
+        if (usersEntity == null){
+            this.setLogin("");
+            this.setPassword("");
+            return "index";
+        } else if(password.equals(usersEntity.getPassword())){
+            return "books";
+        }
+        this.setLogin("");
+        this.setPassword("");
+        return "index";
+    }
+
+    public String createUser() throws Exception{
+        UsersEntity usersEntity = entityManager.find(UsersEntity.class, login);
+        if(usersEntity != null){
+            this.setLogin("");
+            this.setPassword("");
+            return "index";
+        }
+
+        usersEntity = new UsersEntity();
+        usersEntity.setLogin(login);
+        usersEntity.setPassword(password);
+        userTransaction.begin();
+        entityManager.persist(usersEntity);
+        userTransaction.commit();
+        return "books";
+    }
+}
